476a62797a06796a41c7f7b79970bb79684d11da
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-06-16 13:29:42 +0530

    new line added

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -17,6 +17,7 @@ export default function Home() {
         <ProfessionalServices />
         <WhatweOffer />
         <FeatureComponent />
+        
         <ProductDisplay />
         <Choose />
 
