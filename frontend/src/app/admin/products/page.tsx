e44367548f499276fe145ccf400d65f178412834
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-06-21 14:15:06 +0530

    Cards added

--- frontend/src/app/admin/products/page.tsx
+++ frontend/src/app/admin/products/page.tsx
@@ -1,14 +1,16 @@
 import DefaultLayout from '../../components/admin/Layouts/DefaultLayout';
 import ProductsTable from '@/components/Products/productsTable';
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-
+import ProductPageCards from "@/components/Products/cards/productpageCards"; // Corrected import statement
 const ProjectTable = () => {
   return (
 
-      <DefaultLayout> 
-        <Breadcrumb pageName='Products'/>
-          <ProductsTable />
-      </DefaultLayout>
+    <DefaultLayout>
+      <Breadcrumb pageName='Products' />
+      <ProductPageCards />
+
+      <ProductsTable />
+    </DefaultLayout>
 
   );
 };
