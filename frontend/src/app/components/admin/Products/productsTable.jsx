532b9c0cd2832029245329eeb0fbc76da44b932b
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-06-21 14:01:42 +0530

    additional css realted changes

--- frontend/src/app/components/admin/Products/productsTable.jsx
+++ frontend/src/app/components/admin/Products/productsTable.jsx
@@ -58,6 +58,7 @@ const TableTwo = () => {
   }
 
 
+
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="px-4 py-6 md:px-6 xl:px-7.5 items-start justify-between md:flex">
@@ -265,7 +266,7 @@ const TableTwo = () => {
       </div>
 
 
-      <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="px-2 rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
         <div className="max-w-full overflow-x-auto">
           <table className="w-full table-auto">
             <thead>
