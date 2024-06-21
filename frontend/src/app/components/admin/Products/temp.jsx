0f348cef06c604efa492fefe3e7f7e9ccd7a65d7
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-06-21 13:54:31 +0530

    Products Table Shifted from Div based layout to Table based layout , becomes responsive even small devices

---
+++ frontend/src/app/components/admin/Products/temp.jsx
@@ -0,0 +1,433 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+const productData = [
+  {
+    image: "/images/product/product-01.png",
+    name: "Apple Watch Series 7",
+    category: "Electronics",
+    price: 296,
+    sold: 22,
+    profit: 45,
+  },
+  {
+    image: "/images/product/product-02.png",
+    name: "Macbook Pro M1",
+    category: "Electronics",
+    price: 546,
+    sold: 12,
+    profit: 125,
+  },
+  {
+    image: "/images/product/product-03.png",
+    name: "Dell Inspiron 15",
+    category: "Electronics",
+    price: 443,
+    sold: 64,
+    profit: 247,
+  },
+  {
+    image: "/images/product/product-04.png",
+    name: "HP Probook 450",
+    category: "Electronics",
+    price: 499,
+    sold: 72,
+    profit: 103,
+  },
+];
+
+const TableTwo = () => {
+  const [checkedItems, setCheckedItems] = useState([]);
+
+  const handleCheckboxChange = (index) => {
+    setCheckedItems((prev) => {
+      if (prev.includes(index)) {
+        return prev.filter((item) => item !== index);
+      } else {
+        return [...prev, index];
+      }
+    });
+  };
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+
+  return (
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="px-4 py-6 md:px-6 xl:px-7.5 items-start justify-between md:flex">
+        <div className="max-w-lg">
+          <h4 className="text-xl font-semibold text-black dark:text-white">
+            Top Products
+          </h4>
+
+          <p className="text-gray-600 mt-2">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </p>
+        </div>
+      </div>
+      <div className="px-4 py-6 md:px-6 xl:px-7.5 items-start justify-between md:flex space-x-4">
+  <h4 className="text-xl font-semibold text-black dark:text-white">
+    Products 
+  </h4>
+
+  <div className="flex space-x-3 items-center">
+    <form onSubmit={(e) => e.preventDefault()} className="px-4 mx-auto">
+      <div className="relative">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute top-0 bottom-0 w-4 h-4 my-auto text-gray-400 left-3"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-80 py-1 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+        />
+      </div>
+    </form>
+
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Options
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </MenuButton>
+      </div>
+      <Transition
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <MenuItem>
+              {({ focus }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Account settings
+                </a>
+              )}
+            </MenuItem>
+            <MenuItem>
+              {({ focus }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Support
+                </a>
+              )}
+            </MenuItem>
+            <MenuItem>
+              {({ focus }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  License
+                </a>
+              )}
+            </MenuItem>
+            <form method="POST" action="#">
+              <MenuItem>
+                {({ focus }) => (
+                  <button
+                    type="submit"
+                    className={classNames(
+                      focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block w-full px-4 py-2 text-left text-sm'
+                    )}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </MenuItem>
+            </form>
+          </div>
+        </MenuItems>
+      </Transition>
+    </Menu>
+
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Options
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </MenuButton>
+      </div>
+      <Transition
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <MenuItem>
+              {({ focus }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Account settings
+                </a>
+              )}
+            </MenuItem>
+            <MenuItem>
+              {({ focus }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Support
+                </a>
+              )}
+            </MenuItem>
+            <MenuItem>
+              {({ focus }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  License
+                </a>
+              )}
+            </MenuItem>
+            <form method="POST" action="#">
+              <MenuItem>
+                {({ focus }) => (
+                  <button
+                    type="submit"
+                    className={classNames(
+                      focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block w-full px-4 py-2 text-left text-sm'
+                    )}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </MenuItem>
+            </form>
+          </div>
+        </MenuItems>
+      </Transition>
+    </Menu>
+
+    <div>
+      <a
+        href="javascript:void(0)"
+        className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
+      >
+        Add Product
+      </a>
+    </div>
+  </div>
+</div>
+
+
+
+      <div className="grid grid-cols-8 border-t border-stroke px-4 py-4.5 dark:border-strokedark md:px-6 2xl:px-7.5">
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Select</p>
+        </div>
+        <div className="col-span-2 flex items-center">
+          <p className="font-medium">Product Name</p>
+        </div>
+        <div className="col-span-1 hidden items-center sm:flex">
+          <p className="font-medium">Category</p>
+        </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Price</p>
+        </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Sold</p>
+        </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Profit</p>
+        </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Actions</p>
+        </div>
+      </div>
+
+      {productData.map((product, key) => (
+        <div
+          className="grid grid-cols-8 border-t border-stroke px-4 py-4.5 dark:border-strokedark md:px-6 2xl:px-7.5"
+          key={key}
+        >
+          <div className="col-span-1 flex items-center">
+            <label
+              htmlFor={`checkbox${key}`}
+              className="flex cursor-pointer select-none items-center"
+            >
+              <input
+                type="checkbox"
+                id={`checkbox${key}`}
+                className="sr-only"
+                checked={checkedItems.includes(key)}
+                onChange={() => handleCheckboxChange(key)}
+              />
+              <div
+                className={`mr-4 flex h-5 w-5 items-center justify-center rounded border ${checkedItems.includes(key)
+                  ? "border-primary bg-gray dark:bg-transparent"
+                  : "border-gray-300"
+                  }`}
+              >
+                {checkedItems.includes(key) && (
+                  <svg
+                    width="11"
+                    height="8"
+                    viewBox="0 0 11 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
+                      fill="#3056D3"
+                      stroke="#3056D3"
+                      strokeWidth="0.4"
+                    ></path>
+                  </svg>
+                )}
+              </div>
+            </label>
+          </div>
+          <div className="col-span-2 flex items-center">
+            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+              <div className="h-12.5 w-15 rounded-md">
+                <Image src={product.image} width={60} height={50} alt="Product" />
+              </div>
+              <p className="text-sm text-black dark:text-white">
+                {product.name}
+              </p>
+            </div>
+          </div>
+          <div className="col-span-1 hidden items-center sm:flex">
+            <p className="text-sm text-black dark:text-white">{product.category}</p>
+          </div>
+          <div className="col-span-1 flex items-center">
+            <p className="text-sm text-black dark:text-white">${product.price}</p>
+          </div>
+          <div className="col-span-1 flex items-center">
+            <p className="text-sm text-black dark:text-white">{product.sold}</p>
+          </div>
+          <div className="col-span-1 flex items-center">
+            <p className="text-sm text-meta-3">${product.profit}</p>
+          </div>
+          <div className="col-span-1 flex items-center space-x-3.5">
+            <button onClick={() => openModal(user)} className="hover:text-primary">
+              <svg
+                className="fill-current"
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z"
+                  fill=""
+                />
+                <path
+                  d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z"
+                  fill=""
+                />
+              </svg>
+            </button>
+            {/* Add a button to trigger the modal */}
+            {/* onClick={() => setIsDeleteModalOpen(true)} */}
+            <button className="hover:text-primary" >
+              <svg
+                className="fill-current"
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13.7535 2.47502H11.5879V1.9969C11.5879 1.15315 10.9129 0.478149 10.0691 0.478149H7.90352C7.05977 0.478149 6.38477 1.15315 6.38477 1.9969V2.47502H4.21914C3.40352 2.47502 2.72852 3.15002 2.72852 3.96565V4.8094C2.72852 5.42815 3.09414 5.9344 3.62852 6.1594L4.07852 15.4688C4.13477 16.6219 5.09102 17.5219 6.24414 17.5219H11.7004C12.8535 17.5219 13.8098 16.6219 13.866 15.4688L14.3441 6.13127C14.8785 5.90627 15.2441 5.3719 15.2441 4.78127V3.93752C15.2441 3.15002 14.5691 2.47502 13.7535 2.47502ZM7.67852 1.9969C7.67852 1.85627 7.79102 1.74377 7.93164 1.74377H10.0973C10.2379 1.74377 10.3504 1.85627 10.3504 1.9969V2.47502H7.70664V1.9969H7.67852ZM4.02227 3.96565C4.02227 3.85315 4.10664 3.74065 4.24727 3.74065H13.7535C13.866 3.74065 13.9785 3.82502 13.9785 3.96565V4.8094C13.9785 4.9219 13.8941 5.0344 13.7535 5.0344H4.24727C4.13477 5.0344 4.02227 4.95002 4.02227 4.8094V3.96565ZM11.7285 16.2563H6.27227C5.79414 16.2563 5.40039 15.8906 5.37227 15.3844L4.95039 6.2719H13.0785L12.6566 15.3844C12.6004 15.8625 12.2066 16.2563 11.7285 16.2563Z"
+                  fill=""
+                />
+                <path
+                  d="M9.00039 9.11255C8.66289 9.11255 8.35352 9.3938 8.35352 9.75942V13.3313C8.35352 13.6688 8.63477 13.9782 9.00039 13.9782C9.33789 13.9782 9.64727 13.6969 9.64727 13.3313V9.75942C9.64727 9.3938 9.33789 9.11255 9.00039 9.11255Z"
+                  fill=""
+                />
+                <path
+                  d="M11.2502 9.67504C10.8846 9.64692 10.6033 9.90004 10.5752 10.2657L10.4064 12.7407C10.3783 13.0782 10.6314 13.3875 10.9971 13.4157C11.0252 13.4157 11.0252 13.4157 11.0533 13.4157C11.3908 13.4157 11.6721 13.1625 11.6721 12.825L11.8408 10.35C11.8408 9.98442 11.5877 9.70317 11.2502 9.67504Z"
+                  fill=""
+                />
+                <path
+                  d="M6.72245 9.67504C6.38495 9.70317 6.1037 10.0125 6.13182 10.35L6.3287 12.825C6.35683 13.1625 6.63808 13.4157 6.94745 13.4157C6.97558 13.4157 6.97558 13.4157 7.0037 13.4157C7.3412 13.3875 7.62245 13.0782 7.59433 12.7407L7.39745 10.2657C7.39745 9.90004 7.08808 9.64692 6.72245 9.67504Z"
+                  fill=""
+                />
+              </svg>
+            </button>
+            <button className="hover:text-primary">
+              <svg
+                className="fill-current"
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16.8754 11.6719C16.5379 11.6719 16.2285 11.9531 16.2285 12.3187V14.8219C16.2285 15.075 16.0316 15.2719 15.7785 15.2719H2.22227C1.96914 15.2719 1.77227 15.075 1.77227 14.8219V12.3187C1.77227 11.9812 1.49102 11.6719 1.12539 11.6719C0.759766 11.6719 0.478516 11.9531 0.478516 12.3187V14.8219C0.478516 15.7781 1.23789 16.5375 2.19414 16.5375H15.7785C16.7348 16.5375 17.4941 15.7781 17.4941 14.8219V12.3187C17.5223 11.9531 17.2129 11.6719 16.8754 11.6719Z"
+                  fill=""
+                />
+                <path
+                  d="M8.55074 12.3469C8.66324 12.4594 8.83199 12.5156 9.00074 12.5156C9.16949 12.5156 9.31012 12.4594 9.45074 12.3469L13.4726 8.43752C13.7257 8.1844 13.7257 7.79065 13.5007 7.53752C13.2476 7.2844 12.8539 7.2844 12.6007 7.5094L9.64762 10.4063V2.1094C9.64762 1.7719 9.36637 1.46252 9.00074 1.46252C8.66324 1.46252 8.35387 1.74377 8.35387 2.1094V10.4063L5.40074 7.53752C5.14762 7.2844 4.75387 7.31252 4.50074 7.53752C4.24762 7.79065 4.27574 8.1844 4.50074 8.43752L8.55074 12.3469Z"
+                  fill=""
+                />
+              </svg>
+            </button>
+
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TableTwo;
--- frontend/src/app/components/admin/Tables/TableThree.tsx
+++ frontend/src/app/components/admin/Tables/TableThree.tsx
@@ -29,7 +29,7 @@ const packageData: Package[] = [
 
 const TableThree = () => {
   return (
-    <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
           <thead>
